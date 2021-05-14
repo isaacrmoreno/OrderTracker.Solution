706c22d361e8ad0264c507f089cdf2c384f843cd
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-14 17:47:57 -0600

    start testing for orders

--- OrderTracker.Tests/ModelTests/OrderTest.cs
+++ OrderTracker.Tests/ModelTests/OrderTest.cs
@@ -1,16 +1,24 @@
-// using System;
-// using System.Collections.Generic;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using PierresBakery.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
 
-// namespace PierresBakery.TestTools
-// {
-//   [TestClass]
-//   public class OrderTest
-//   {
-//     [TestMethod]
-//     {
-// // Code Goes here
-//     }
-//   }
-// }
+namespace PierresBakery.TestTools
+{
+  [TestClass]
+  public class OrderTest : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    [TestMethod]
+    {
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test order");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
+}

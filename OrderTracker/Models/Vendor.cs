55d7e7e6d2c817022b14ab0f20a8b2c960f57fb8
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-14 11:33:23 -0600

    typo fix

--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Vendor
   {
-
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
   }
 }

13058bb843fb737935f082db9d13f59aacfaa47b
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-14 12:33:52 -0600

    commented out boiler plate while I focus on vendor test

--- OrderTracker.Tests/ModelTests/OrderTest.cs
+++ OrderTracker.Tests/ModelTests/OrderTest.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PierresBakery.Models;
+// using System;
+// using System.Collections.Generic;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using PierresBakery.Models;
 
-namespace PierresBakery.TestTools
-{
-  [TestClass]
-  public class OrderTest
-  {
-    [TestMethod]
-    public void GameConstructor_CreatesInstanceOfHangmanGame_HangmanGame()
-    {
-      HangmanGame newHangman = new();
-      Assert.AreEqual(typeof(HangmanGame), newHangman.GetType());
-    }
-  }
-}
+// namespace PierresBakery.TestTools
+// {
+//   [TestClass]
+//   public class OrderTest
+//   {
+//     [TestMethod]
+//     {
+// // Code Goes here
+//     }
+//   }
+// }

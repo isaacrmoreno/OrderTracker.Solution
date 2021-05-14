2c3e042ba108289b63ffd8f3050e3df1eb979144
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-14 17:26:05 -0600

    updates

--- OrderTracker/Controllers/OrderController.cs
+++ OrderTracker/Controllers/OrderController.cs
@@ -6,10 +6,22 @@ namespace PierresBakery.Controllers
 {
   public class OrderController : Controller
   {
-    [HttpGet("/order")]
-    public ActionResult Index()
+    [HttpGet("/vendors/{vendorId}/orders/orders{orderId}")] // Notsure about filepath?
+    public ActionResult Show(int venderId, int orderId)
     {
-      return View();
+      Order Order = Order.Find(orderId); // havent made a method for it yet and it needs to be tested. 
+      Vendor Vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
     }
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
   }
 }

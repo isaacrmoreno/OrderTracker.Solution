35cba5860d3f75b6e97ebcdeba00f50658dad0e7
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-14 16:16:24 -0600

    added show method

--- OrderTracker/Controllers/VendorController.cs
+++ OrderTracker/Controllers/VendorController.cs
@@ -10,7 +10,8 @@ namespace PierresBakery.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
-      return View();
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
     }
     [HttpGet("/vendors/new")]
     public ActionResult New()
@@ -19,10 +20,20 @@ namespace PierresBakery.Controllers
     }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName)
+    public ActionResult Create(string vendorName, string vendorDescription)
     {
-      Vendor newVendor = new Vendor(vendorName);
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
+    }
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> VendorOrders = selectedVendor.Orders;
+      model.Add("Vendor", selectedVendor);
+      model.Add("Orders", VendorOrders);
+      return View(model);
     }
   }
 }

0fd6835ec2c66e2815821cec7abaf29433aba125
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-14 13:56:18 -0600

    RED: Failed because GetAll method does not exist i yet

--- OrderTracker.Tests/ModelTests/VendorTest.cs
+++ OrderTracker.Tests/ModelTests/VendorTest.cs
@@ -34,5 +34,16 @@ namespace PierresBakery.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_Vendorlist()
+    {
+      string name1 = "Bill Baguettes";
+      string name2 = "Claire's Cupcakes";
+      Vendor newVender1 = new Vendor(name1);
+      Vendor newVender2 = new Vendor(name2);
+      List<Vendor> newList = new List<Vendor> { newVender1, newVender2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }

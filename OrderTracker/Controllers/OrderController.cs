461838ab7f8a9280d44be10cb3c99f55f3301b0f
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-16 20:09:58 -0700

    fixed typo

--- OrderTracker/Controllers/OrderController.cs
+++ OrderTracker/Controllers/OrderController.cs
@@ -6,22 +6,21 @@ namespace PierresBakery.Controllers
 {
   public class OrderController : Controller
   {
-    [HttpGet("/vendors/{vendorId}/orders/orders{orderId}")] // Notsure about filepath?
-    public ActionResult Show(int venderId, int orderId)
-    {
-      Order Order = Order.Find(orderId); // havent made a method for it yet and it needs to be tested. 
-      Vendor Vendor = Vendor.Find(vendorId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
-      return View(model);
-    }
     [HttpGet("/vendors/{vendorId}/orders/new")]
     public ActionResult New(int vendorId)
     {
       Vendor vendor = Vendor.Find(vendorId);
       return View(vendor);
     }
-
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
   }
 }

5e0cfe7e37ed0836f2a7acdc1a6fad1c868ae3b5
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-16 20:14:26 -0700

    added title, description, price and date to Create method

--- OrderTracker/Controllers/VendorController.cs
+++ OrderTracker/Controllers/VendorController.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
-using System;
 using Microsoft.AspNetCore.Mvc;
 using PierresBakery.Models;
+using System;
 
 namespace PierresBakery.Controllers
 {
@@ -20,28 +20,28 @@ namespace PierresBakery.Controllers
     }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName, string vendorDescription)
+    public ActionResult Create(string vendorName, string description)
     {
-      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      Vendor newVendor = new(vendorName, description);
       return RedirectToAction("Index");
     }
-    [HttpGet("/categories/{id}")]
+    [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
     {
-      Dictionary<string, object> model = new Dictionary<string, object>();
+      Dictionary<string, object> model = new();
       Vendor selectedVendor = Vendor.Find(id);
       List<Order> VendorOrders = selectedVendor.Orders;
-      model.Add("Vendor", selectedVendor);
-      model.Add("Orders", VendorOrders);
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", VendorOrders);
       return View(model);
     }
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderDescription)
+    public ActionResult Create(int vendorId, string title, string description, int price, int date)
     {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor foundvendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(OrderDescription);
-      foundVendor.AddItem(newOrder);
+      Dictionary<string, object> model = new();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new(title, description, price, date);
+      foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
       model.Add("vendor", foundVendor);

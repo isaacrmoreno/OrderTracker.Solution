403b473134de44e6542c5dbc485ba129c578fd2a
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-16 21:43:37 -0700

    add refactored and passing test for both Vendors and Orders

--- OrderTracker.Tests/ModelTests/OrderTest.cs
+++ OrderTracker.Tests/ModelTests/OrderTest.cs
@@ -13,12 +13,61 @@ namespace PierresBakery.TestTools
       Order.ClearAll();
     }
     [TestMethod]
-    {
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test order");
+      Order newOrder = new Order("Max's Muffins", "Needs lots of rye bread", 3, 1 / 10 / 1997);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetOrderTitle_ReturnsOrderTitle_String()
+    {
+      string orderTitle = "Max's Muffins";
+      Order newOrder = new Order(orderTitle, "Needs lots of rye bread", 3, 1 / 10 / 1997);
+      string result = newOrder.Title;
+      Assert.AreEqual(orderTitle, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Needs lots of bread";
+      Order newOrder = new Order("Max's Muffins", description, 3, 1 / 10 / 1997);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Int()
+    {
+
+      int price = 3;
+      Order newOrder = new Order("Max's Muffins", "Needs lots of rye bread", price, 1 / 10 / 1997);
+
+      int result = newOrder.Price;
+
+      Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetDate_ReturnsDate_String()
+    {
+
+      int date = 1 / 10 / 1997;
+      Order newOrder = new Order("Max's Muffins", "Needs lots of rye bread", 3, date);
+
+      int result = newOrder.Date;
+
+      Assert.AreEqual(date, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      Order newOrder1 = new Order("Max's Muffins", "Needs lots of rye bread", 3, 1 / 10 / 1997);
+      Order newOrder2 = new Order("Sally's Scones", "Toast Baguettes to a Crisp", 4, 2 / 7 / 1995);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
-}
 }
--- OrderTracker.Tests/ModelTests/VendorTest.cs
+++ OrderTracker.Tests/ModelTests/VendorTest.cs
@@ -15,14 +15,14 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test vendor");
+      Vendor newVendor = new Vendor("Bills Baguettes", "Expedite Shipping");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]
     public void GetVendorName_ReturnsName_String()
     {
       string name = "Bills Baguettes";
-      Vendor newVender = new Vendor(name);
+      Vendor newVender = new Vendor(name, "Expedite Shipping");
       string result = newVender.Name;
       Assert.AreEqual(name, result);
     }
@@ -30,7 +30,7 @@ namespace PierresBakery.Tests
     public void GetId_ReturnsVendorId_int()
     {
       string name = "Claire's Cupcakes";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Add Extra Frosting");
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
@@ -39,8 +39,8 @@ namespace PierresBakery.Tests
     {
       string name1 = "Bill Baguettes";
       string name2 = "Claire's Cupcakes";
-      Vendor newVender1 = new Vendor(name1);
-      Vendor newVender2 = new Vendor(name2);
+      Vendor newVender1 = new Vendor(name1, "Expedite Shipping");
+      Vendor newVender2 = new Vendor(name2, "Add Extra Frosting");
       List<Vendor> newList = new List<Vendor> { newVender1, newVender2 };
       List<Vendor> result = Vendor.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -50,19 +50,20 @@ namespace PierresBakery.Tests
     {
       string name1 = "Bill Baguettes";
       string name2 = "Claire's Cupcakes";
-      Vendor newVender1 = new Vendor(name1);
-      Vendor newVender2 = new Vendor(name2);
+      Vendor newVender1 = new Vendor(name1, "Expedite Shipping");
+      Vendor newVender2 = new Vendor(name2, "Add Extra Frosting");
       Vendor result = Vendor.Find(2);
       Assert.AreEqual(newVender2, result);
     }
     [TestMethod]
     public void AddOrder_AssociatesOrderWithVendor_OrderList()
     {
+
       string description = "Needs 5 loafs of bread";
-      Order newOrder = new Order(description);
+      Order newOrder = new Order("Max's Muffins", description, 3, 1 / 10 / 1997);
       List<Order> newList = new List<Order> { newOrder };
       string name = "Max's Muffins";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "Muffins on Sale");
       newVendor.AddOrder(newOrder);
       List<Order> result = newVendor.Orders;
       CollectionAssert.AreEqual(newList, result);

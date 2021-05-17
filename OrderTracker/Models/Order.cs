995b86f0f4e10492af5d7cc30506bc8233685113
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-16 20:15:13 -0700

    added get all and find methods

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -4,21 +4,32 @@ namespace PierresBakery.Models
 {
   public class Order
   {
-    private static List<Order> _instances = new List<Order> { };
+    private static List<Order> _instances = new() { };
     public string Title { get; set; }
     public string Description { get; set; }
     public int Price { get; set; }
     public int Date { get; set; }
     public int Id { get; }
-    public Order(string description)
+    public Order(string title, string description, int price, int date)
     {
+      Title = title;
       Description = description;
+      Price = price;
+      Date = date;
       _instances.Add(this);
       Id = _instances.Count;
+    }
+    public static List<Order> GetAll()
+    {
+      return _instances;
     }
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
   }
 }
